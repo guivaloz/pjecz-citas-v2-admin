42e7707fb795cbc3c990363630b3bb88c22346bf
Guillermo Valdes <guillermo.valdes@pjecz.gob.mx>
2022-05-16 10:53:31 -0500

    Se mejora el modelo CitClienteRegistro

--- citas_admin/blueprints/cit_clientes_registros/models.py
+++ citas_admin/blueprints/cit_clientes_registros/models.py
@@ -17,13 +17,13 @@ class CitClienteRegistro(db.Model, UniversalMixin):
     # Columnas
     nombres = db.Column(db.String(256), nullable=False)
     apellido_primero = db.Column(db.String(256), nullable=False)
-    apellido_segundo = db.Column(db.String(256), default="", server_default="")
-    curp = db.Column(db.String(18), unique=True, nullable=False)
-    telefono = db.Column(db.String(64), default="", server_default="")
-    email = db.Column(db.String(256), unique=True, nullable=False)
+    apellido_segundo = db.Column(db.String(256), nullable=False, default="", server_default="")
+    curp = db.Column(db.String(18), nullable=False, index=True)
+    telefono = db.Column(db.String(64), nullable=False, default="", server_default="")
+    email = db.Column(db.String(256), nullable=False, index=True)
     expiracion = db.Column(db.DateTime(), nullable=False)
     cadena_validar = db.Column(db.String(256), nullable=False)
-    ya_registrado = db.Column(db.Boolean(), default=False, server_default="false")
+    ya_registrado = db.Column(db.Boolean(), nullable=False, default=False, server_default="false")
 
     def __repr__(self):
         """Representación"""
