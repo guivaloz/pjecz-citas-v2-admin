423e167f9957f982247bbaa507143c3c8bc85aaf
Guillermo Valdes <guillermo.valdes@pjecz.gob.mx>
2022-05-17 09:59:58 -0500

    Comienza cmd cit clientes registros

--- cli/commands/cmd_cit_clientes_registros.py
+++ cli/commands/cmd_cit_clientes_registros.py
@@ -1,10 +1,20 @@
 """
 Cit Clientes Registros
 
-- agregar: Agregar manualmente un nuevo cliente
-- agregar_aleatorios: Agregar aleatoriamente muchos clientes
+- ver: Ver mensaje de registro
+- enviar: Enviar mensaje de registro
+- cancelar: Cancelar mensaje de registro
 """
 import click
+from tabulate import tabulate
+
+from citas_admin.app import create_app
+from citas_admin.extensions import db
+
+from citas_admin.blueprints.cit_clientes_registros.models import CitClienteRegistro
+
+app = create_app()
+db.app = app
 
 
 @click.group()
@@ -13,16 +23,40 @@ def cli():
 
 
 @click.command()
-def agregar():
-    """Agregar manualmente un nuevo cliente"""
+@click.option("--id", default=1, help="Number of greetings.")
+def ver():
+    """Ver mensaje de registro"""
+    cit_clientes_registros = CitClienteRegistro.query.filter_by(estatus="A").filter_by(ya_registrado=False).all()
+    if cit_clientes_registros is None:
+        click.echo("No hay registros")
+        return
+    datos = []
+    for cit_cliente_registro in cit_clientes_registros:
+        datos.append(
+            [
+                cit_cliente_registro.id,
+                cit_cliente_registro.nombres,
+                cit_cliente_registro.apellido_primero,
+                cit_cliente_registro.apellido_segundo,
+                cit_cliente_registro.email,
+                cit_cliente_registro.curp,
+            ]
+        )
+    click.echo(tabulate(datos, headers=["id", "nombres", "apellido_primero", "apellido_segundo", "email", "curp"]))
+
+
+@click.command()
+def enviar():
+    """Enviar mensaje de registro"""
     click.echo("Por programar")
 
 
 @click.command()
-def agregar_aleatorios():
-    """Agregar aleatoriamente muchos clientes"""
+def cancelar():
+    """Cancelar mensaje de registro"""
     click.echo("Por programar")
 
 
-cli.add_command(agregar)
-cli.add_command(agregar_aleatorios)
+cli.add_command(ver)
+cli.add_command(enviar)
+cli.add_command(cancelar)
