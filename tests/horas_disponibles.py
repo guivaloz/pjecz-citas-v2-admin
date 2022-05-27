e605c69ee11bea1f3b8e867a37781ebf270f2928
Ricardo Valdes <ricardo.valdes@users.noreply.github.com>
2022-05-27 15:10:22 -0500

    ultimos cambios

--- tests/horas_disponibles.py
+++ tests/horas_disponibles.py
@@ -2,7 +2,7 @@
 Lectura de la Base de datos de la versión 1.0
 """
 from dotenv import load_dotenv
-from datetime import date, timedelta
+from datetime import date, timedelta, datetime
 
 from citas_admin.app import create_app
 from citas_admin.extensions import db
@@ -25,39 +25,70 @@ def main():
     print("=== SIMULACIÓN DE API ENTREGANDO HORAS-DISPONIBLES ===")
     print("======================================================")
     print("")
-    if False:
-        # Listar los distritos diponibles
-        distritos = Distrito.query.filter(Distrito.estatus == "A").all()
-        print("--- Listado de Distritos ---")
-        print("- ID | Nombre              -")
-        print("----------------------------")
-        for distrito in distritos:
-            print(f" {distrito.id:>3} | {distrito.nombre_corto}")
-        print("----------------------------")
-        distrito_id = input("Seleccione un distrito ID: ")
 
-        # Listar los Oficinas del Distrito Seleccionado
-        oficinas = Oficina.query.filter(Oficina.distrito_id == distrito_id).filter(Oficina.estatus == "A").all()
-        print("--- Listado de Oficinas ------")
-        print(f"- ID | {'Clave':<16} | {'Nombre':<30} -")
-        print(f"{'':-^60}")
-        for oficina in oficinas:
-            print(f" {oficina.id:>3} | {oficina.clave:<16} | {oficina.descripcion_corta}")
-        print(f"{'':-^60}")
-        oficina_id = input("Seleccione una oficina ID: ")
+    # Listar los distritos diponibles
+    distrito = None
+    distritos = Distrito.query.filter(Distrito.estatus == "A").all()
+    print("--- Listado de Distritos ---")
+    print("- ID | Nombre              -")
+    print("----------------------------")
+    for distrito in distritos:
+        print(f" {distrito.id:>3} | {distrito.nombre_corto}")
+    print("----------------------------")
+    while True:
+        distrito_id = int(input("Seleccione un distrito ID: "))
+        distrito = Distrito.query.get(distrito_id)
+        if distrito:
+            print(f" ID: {distrito.id} : {distrito.nombre_corto}")
+            break
+        else:
+            print("! Distrito inválido, seleccione otro.")
 
-        # Listar los servicios de la Oficina Seleccionada
-        cit_servicios = CitServicio.query.join(CitOficinaServicio).filter(CitOficinaServicio.oficina_id == oficina_id).filter(CitServicio.estatus == "A").all()
-        print("--- Listado de Servicios ------")
-        print(f"- ID | {'Clave':<16} | {'Nombre':<30} -")
-        print(f"{'':-^60}")
-        for cit_servicio in cit_servicios:
-            print(f" {cit_servicio.id:>3} | {cit_servicio.clave:<16} | {cit_servicio.descripcion}")
-        print(f"{'':-^60}")
-        cit_servicio_id = input("Seleccione un Servicio ID: ")
+    # Listar los Oficinas del Distrito Seleccionado
+    oficina = None
+    oficinas = Oficina.query.filter(Oficina.distrito_id == distrito_id).filter(Oficina.estatus == "A").all()
+    oficinas_disponibles = []
+    print("--- Listado de Oficinas ------")
+    print(f"- ID | {'Clave':<16} | {'Nombre':<30} -")
+    print(f"{'':-^60}")
+    for oficina in oficinas:
+        print(f" {oficina.id:>3} | {oficina.clave:<16} | {oficina.descripcion_corta}")
+        oficinas_disponibles.append(oficina.id)
+    print(f"{'':-^60}")
+    while True:
+        oficina_id = int(input("Seleccione una Oficina ID: "))
+        if oficina_id in oficinas_disponibles:
+            oficina = Oficina.query.get(oficina_id)
+            if oficina:
+                print(f" ID: {oficina.id} : {oficina.clave} | {oficina.descripcion_corta}")
+                break
+        else:
+            print("! Oficina inválida, seleccione otra.")
+
+    # Listar los servicios de la Oficina Seleccionada
+    cit_servicio = None
+    cit_servicios = CitServicio.query.join(CitOficinaServicio).filter(CitOficinaServicio.oficina_id == oficina_id).filter(CitServicio.estatus == "A").all()
+    cit_servicios_disponibles = []
+    print("--- Listado de Servicios ------")
+    print(f"- ID | {'Clave':<16} | {'Nombre':<30} -")
+    print(f"{'':-^60}")
+    for cit_servicio in cit_servicios:
+        print(f" {cit_servicio.id:>3} | {cit_servicio.clave:<16} | {cit_servicio.descripcion}")
+        cit_servicios_disponibles.append(cit_servicio.id)
+    print(f"{'':-^60}")
+    while True:
+        cit_servicio_id = int(input("Seleccione un Servicio ID: "))
+        if cit_servicio_id in cit_servicios_disponibles:
+            cit_servicio = CitServicio.query.get(cit_servicio_id)
+            if cit_servicio:
+                print(f" ID: {cit_servicio.id} : {cit_servicio.clave} | {cit_servicio.descripcion}")
+                break
+        else:
+            print("! Servicio inválido, seleccione otro.")
 
     # Listar fechas disponibles
     LIMITE_DIAS = 90
+    fecha = None
     dias_disponibles = []
     # consultar días inhábiles
     dias_inhabiles_obj = CitDiaInhabil.query.filter(CitDiaInhabil.fecha > date.today()).filter(CitDiaInhabil.estatus == "A").all()
@@ -79,14 +110,35 @@ def main():
     for dia in dias_disponibles:
         print(f" {dia}", end ="\t")
         col += 1
-        if col > 5:
+        if col > 4:
             print()
             col = 0
     print()
     print("------------")
-    fecha = input("Seleccione una Fecha (YYYY-MM-DD): ")
+    while True:
+        fecha_str = input("Seleccione una Fecha (YYYY-MM-DD):")
+        print(fecha_str)
+        fecha = datetime.strptime(fecha_str, '%Y-%m-%d')
+        if fecha in dias_disponibles:
+            print(f" Día Seleccionado: {fecha_str}")
+            break
+        else:
+            print("! Fecha inválida, seleccione otra.")
+
+    # Listar horas disponibles
+    hora = None
+    horas_disponibles = []
+    # consultar días inhábiles
 
 
+    # Impresión de los valores seleccionados
+    print(f"{'':-^60}")
+    print("--- Cita Programada con éxito ---")
+    print(f"Distrito: {distrito.nombre_corto}")
+    print(f"Oficina: {oficina.clave} | {oficina.descripcion_corta}")
+    print(f"Servicio: {cit_servicio.clave} | {oficina.descripcion}")
+    print(f"Fecha: {fecha.strftime('%Y-%m-%d')}, Hora: {hora}")
+    print(f"{'':-^60}")
 
 if __name__ == "__main__":
     main()
