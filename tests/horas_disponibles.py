6e4c9c540648ecf10e881fe05b5259b9f99183e2
Ricardo Valdes <ricardo.valdes@users.noreply.github.com>
2022-05-31 14:07:37 -0500

    Simulación de horas disponibles para una nueva cita

--- tests/horas_disponibles.py
+++ tests/horas_disponibles.py
@@ -2,7 +2,7 @@
 Lectura de la Base de datos de la versión 1.0
 """
 from dotenv import load_dotenv
-from datetime import date, timedelta, datetime
+from datetime import date, timedelta, datetime, time
 
 from citas_admin.app import create_app
 from citas_admin.extensions import db
@@ -12,6 +12,8 @@ from citas_admin.blueprints.oficinas.models import Oficina
 from citas_admin.blueprints.cit_servicios.models import CitServicio
 from citas_admin.blueprints.cit_oficinas_servicios.models import CitOficinaServicio
 from citas_admin.blueprints.cit_dias_inhabiles.models import CitDiaInhabil
+from citas_admin.blueprints.cit_horas_bloqueadas.models import CitHoraBloqueada
+from citas_admin.blueprints.cit_citas.models import CitCita
 
 def main():
     """Main function"""
@@ -117,10 +119,12 @@ def main():
     print("------------")
     while True:
         fecha_str = input("Seleccione una Fecha (YYYY-MM-DD):")
-        print(fecha_str)
-        fecha = datetime.strptime(fecha_str, '%Y-%m-%d')
+        try:
+            fecha = datetime.strptime(fecha_str, '%Y-%m-%d').date()
+        except:
+            print("! Formato incorrecto de la fecha.")
         if fecha in dias_disponibles:
-            print(f" Día Seleccionado: {fecha_str}")
+            print(f" Día Seleccionado: {fecha}")
             break
         else:
             print("! Fecha inválida, seleccione otra.")
@@ -128,8 +132,55 @@ def main():
     # Listar horas disponibles
     hora = None
     horas_disponibles = []
-    # consultar días inhábiles
 
+    # Calcular horarios
+    inicio = datetime.strptime('08:30:00', '%H:%M:%S') #oficina.apertura
+    final = datetime.strptime('16:30:00', '%H:%M:%S') #oficina.cierre
+    intervalo = datetime.strptime('00:15:00', '%H:%M:%S') #cit_servicio.duracion
+
+    # consultar horas bloquedas
+    horas_bloqueadas_obj = CitHoraBloqueada.query.filter(CitHoraBloqueada.fecha == fecha).filter(CitHoraBloqueada.estatus == "A").all()
+    horas_bloqueadas = [item.hora for item in horas_bloqueadas_obj]
+
+    # Revisar citas previas
+    citas = CitCita.query.filter(CitCita.inicio == fecha).filter(CitCita.estatus == "A").all()
+    horas_ocupadas = [cita.inicio for cita in citas]
+
+    hora = inicio
+    while hora <= final:
+        # Quita las horas bloqueadas
+        if hora in horas_bloqueadas:
+            continue
+        # Quita las horas ocupadas
+        if hora in horas_ocupadas:
+            continue
+        horas_disponibles.append(hora.time())
+        hora = hora + timedelta(hours=intervalo.hour, minutes=intervalo.minute)
+
+    # Desplegar el listado de horarios disponibles
+    print("--- Listado de Horas Disponibles ------")
+    print("- Horas -")
+    print("------------")
+    col = 0
+    for hora in horas_disponibles:
+        print(f" {hora}", end ="\t")
+        col += 1
+        if col > 4:
+            print()
+            col = 0
+    print()
+    print("------------")
+    while True:
+        hora_str = input("Seleccione una Hora (H:M:S):")
+        try:
+            hora = datetime.strptime(hora_str, '%H:%M:%S').time()
+        except:
+            print("! Formato incorrecto de la hora.")
+        if hora in horas_disponibles:
+            print(f" Hora Seleccionada: {hora}")
+            break
+        else:
+            print("! Hora inválida, seleccione otra.")
 
     # Impresión de los valores seleccionados
     print(f"{'':-^60}")
@@ -139,6 +190,7 @@ def main():
     print(f"Servicio: {cit_servicio.clave} | {oficina.descripcion}")
     print(f"Fecha: {fecha.strftime('%Y-%m-%d')}, Hora: {hora}")
     print(f"{'':-^60}")
+
 
 if __name__ == "__main__":
     main()
