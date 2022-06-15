02c2afcf6e4d5e2ce4aa71062a33e9412cba2396
Ricardo Valdes <ricardo.valdes@users.noreply.github.com>
2022-06-15 11:25:41 -0500

    Lista la vista de recepcionista en el módulo de horas bloqueadas

--- citas_admin/blueprints/cit_horas_bloqueadas/forms.py
+++ citas_admin/blueprints/cit_horas_bloqueadas/forms.py
@@ -6,10 +6,21 @@ from wtforms import StringField, SubmitField, DateField, TimeField, SelectField
 from wtforms.validators import DataRequired, Optional
 
 
-class CitHoraBloqueadaForm(FlaskForm):
-    """Formulario CitDíasInhabiles"""
+class CitHoraBloqueadaAdminForm(FlaskForm):
+    """Formulario CitHoraBloqueda"""
 
     oficina = SelectField(label="Oficina", coerce=int, validate_choice=False)
+    fecha = DateField("Fecha", validators=[DataRequired()])
+    inicio_tiempo = TimeField("Tiempo de inicio", validators=[DataRequired()])
+    termino_tiempo = TimeField("Tiempo de termino", validators=[DataRequired()])
+    descripcion = StringField("Descripción", validators=[Optional()])
+    guardar = SubmitField("Guardar")
+
+
+class CitHoraBloqueadaForm(FlaskForm):
+    """Formulario CitHoraBloqueda"""
+
+    oficina = StringField(label="Oficina")  # Read Only
     fecha = DateField("Fecha", validators=[DataRequired()])
     inicio_tiempo = TimeField("Tiempo de inicio", validators=[DataRequired()])
     termino_tiempo = TimeField("Tiempo de termino", validators=[DataRequired()])
