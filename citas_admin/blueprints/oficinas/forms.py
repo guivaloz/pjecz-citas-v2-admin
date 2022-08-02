abfcc19252ed6446e7e3d1688ce513d8ac7f8c36
RicVal <none@none.com>
2022-08-02 09:25:53 -0500

    correccion

--- citas_admin/blueprints/oficinas/forms.py
+++ citas_admin/blueprints/oficinas/forms.py
@@ -2,7 +2,6 @@
 Oficinas, formularios
 """
 from flask_wtf import FlaskForm
-from sqlalchemy import true
 from wtforms import StringField, SubmitField, BooleanField, TimeField, IntegerField
 from wtforms.validators import DataRequired, Length, Optional
 from wtforms.ext.sqlalchemy.fields import QuerySelectField
