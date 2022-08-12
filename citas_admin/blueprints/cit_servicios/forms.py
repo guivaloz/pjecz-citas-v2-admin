fc613fbb8ef19402e914a8ee0735d04f55b2012c
Guillermo Valdes <guillermo.valdes@pjecz.gob.mx>
2022-08-12 11:01:36 -0500

    Listas las correcciones

--- citas_admin/blueprints/cit_servicios/forms.py
+++ citas_admin/blueprints/cit_servicios/forms.py
@@ -9,12 +9,12 @@ from wtforms.validators import DataRequired, Length, Optional
 class CitServicioForm(FlaskForm):
     """Formulario CITDíasInhabiles"""
 
-    cit_categoria_nombre = StringField("Categoria")  # Read only
+    cit_categoria_nombre = StringField("Categoría")  # Read only
     clave = StringField("Clave", validators=[DataRequired(), Length(max=32)])
-    descripcion = StringField("Descripcion", validators=[DataRequired(), Length(max=64)])
+    descripcion = StringField("Descripción", validators=[DataRequired(), Length(max=64)])
     duracion = TimeField("Duración (horas:minutos)", validators=[DataRequired()], format="%H:%M")
-    documentos_limite = IntegerField("Documentos limite", validators=[Optional()])
+    documentos_limite = IntegerField("Cantidad límite de documentos", validators=[Optional()])
     desde = TimeField("Horario de comienzo para agendar citas (horas:minutos)", validators=[Optional()])
-    hasta = TimeField("Horario de termino para agendar citas (horas:minutos)", validators=[Optional()])
-    dias_habilitados = StringField("Días Hábiles", validators=[Optional()])
+    hasta = TimeField("Horario de término para agendar citas (horas:minutos)", validators=[Optional()])
+    dias_habilitados = StringField("Días habilitados", validators=[Optional()])
     guardar = SubmitField("Guardar")
