8ab5dd5af0861b14a4e2fc115bafa39ae2d327e7
Guillermo Valdes <guillermo.valdes@pjecz.gob.mx>
2022-09-02 09:40:52 -0500

    Correcciones en click

--- cli/commands/cmd_enc_sistemas.py
+++ cli/commands/cmd_enc_sistemas.py
@@ -22,7 +22,7 @@ db.app = app
 
 load_dotenv()  # Take environment variables from .env
 
-RECOVER_ACCOUNT_CONFIRM_URL = os.getenv("RECOVER_ACCOUNT_CONFIRM_URL", "")
+POLL_SYSTEM_URL = os.getenv("POLL_SYSTEM_URL", "")
 
 
 @click.group()
@@ -32,11 +32,14 @@ def cli():
 
 @click.command()
 @click.option("--id", default=None, help="El id de una encuesta en particular.", type=int)
-@click.option("--cliente_id", default=None, help="El id del cliente que desea consultar.", type=int)
+@click.option("--cit_cliente_id", default=None, help="El id del cliente que desea consultar.", type=int)
 @click.option("--estado", show_choices=True, type=click.Choice(["pendiente", "cancelado", "contestado"], case_sensitive=False))
-def consultar(id, cliente_id, estado):
-    """Listado de respuestas de la encuesta"""
-    if estado is not None and id is None and cliente_id is None:
+def consultar(id, cit_cliente_id, estado):
+    """Consultar encuestas de sistemas"""
+    click.echo("Listado de encuestas de sistemas")
+
+    # Si solo viene el estado, mostrar una tabla
+    if estado is not None and id is None and cit_cliente_id is None:
         encuestas = EncSistema.query.filter_by(estatus="A").filter_by(estado=estado.upper()).order_by(EncSistema.id).all()
         if len(encuestas) == 0:
             click.echo("No hay registros")
@@ -50,10 +53,11 @@ def consultar(id, cliente_id, estado):
                     encuesta.cit_cliente.nombre,
                 ]
             )
-        click.echo(tabulate(datos, headers=["id", "cliente_id", "nombre del cliente"]))
+        click.echo(tabulate(datos, headers=["ID", "ID del Cliente", "Nombre del Cliente"]))
         click.echo("------------------------------")
         click.echo(f"Cantidad de encuestas: {len(datos)}")
-    # Imprime la respuesta a la encuesta indicada por su id
+
+    # Si viene el ID, se muestran los datos de esa encuesta
     if id is not None and id > 0:
         encuesta = EncSistema.query.get(id)
         click.echo(f"Enviada: {encuesta.creado.strftime('%Y/%m/%d - %H:%M %p')}")
@@ -64,17 +68,18 @@ def consultar(id, cliente_id, estado):
         click.echo(f"Respuesta_03: {encuesta.respuesta_03}")
         click.echo(f"Estado: {encuesta.estado}")
         return 1
+
     # Imprime todas las respuestas a encuestas que a hecho el cliente indicado
-    if cliente_id is not None and cliente_id > 0:
-        cliente = CitCliente.query.get(cliente_id)
+    if cit_cliente_id is not None and cit_cliente_id > 0:
+        cliente = CitCliente.query.get(cit_cliente_id)
         if cliente is None:
-            click.echo(click.style(f"El cliente con el id '{cliente_id}' no existe.", fg="red"))
+            click.echo(click.style(f"El cliente con el id '{cit_cliente_id}' no existe.", fg="red"))
             return 0
-        encuestas = EncSistema.query.filter_by(cit_cliente_id=cliente_id).filter_by(estatus="A")
+        encuestas = EncSistema.query.filter_by(cit_cliente_id=cit_cliente_id).filter_by(estatus="A")
         if estado is not None:
             encuestas = encuestas.filter_by(estado=estado.upper())
         encuestas = encuestas.order_by(EncSistema.id.desc()).all()
-        click.echo(f"Cliente: {cliente_id} - {cliente.nombre}")
+        click.echo(f"Cliente: {cit_cliente_id} - {cliente.nombre}")
         for encuesta in encuestas:
             click.echo("------------------------------")
             click.echo(f"id: {encuesta.id}")
@@ -87,6 +92,7 @@ def consultar(id, cliente_id, estado):
         click.echo("------------------------------")
         click.echo(f"Cantidad de respuestas: {len(encuestas)}")
         return 1
+
     # Regresa 0 en caso de no tener ningún parámetro
     return 0
 
@@ -112,35 +118,52 @@ def _respuesta_int_to_string(respuesta: int):
 @click.command()
 @click.argument("id", type=int)
 def enviar(id):
-    """Enviar email con URL para abrir la encuesta"""
+    """Enviar mensaje por correo electronico con el URL para abrir la encuesta"""
+    click.echo(f"Por enviar mensaje al cliente con ID {id}")
+
+    # Consultar la encuesta
     encuesta = EncSistema.query.get(id)
     if encuesta is None:
         click.echo(click.style(f"La encuesta con el id '{id}' no existe.", fg="red"))
         return 0
-    click.echo(f"Por enviar un email a: {encuesta.cit_cliente.email}")
-    click.echo(f"Con este URL: {RECOVER_ACCOUNT_CONFIRM_URL}?cadena_validar={encuesta.encode_id()}")
+
+    # Validar que este defino POLL_SYSTEM_URL
+    if POLL_SYSTEM_URL == "":
+        click.echo(click.style("Falta la variable de entorno POLL_SYSTEM_URL", fg="red"))
+        return 0
+
+    # Agregar tarea en el fondo para enviar el mensaje
     # app.task_queue.enqueue(
     #     "citas_admin.blueprints.enc_sistemas.tasks.enviar",
     #     cit_cliente_recuperacion_id=cit_cliente_recuperacion.id,
     # )
-    click.echo("Enviar se está ejecutando en el fondo.")
+
+    # Mostrar mensaje de termino
+    url = f"{POLL_SYSTEM_URL}?hashid={encuesta.encode_id()}"
+    click.echo(f"Se ha enviado un mensaje a {encuesta.cit_cliente.email} con el URL {url}")
 
 
 @click.command()
-@click.option("--cliente_id", help="El id del cliente.", type=int)
-def crear(cliente_id):
-    """Crea un nuevo registro de respuesta en la encuesta del sistema"""
-    # Se revisa si existe el cliente
-    cliente = CitCliente.query.get(cliente_id)
+@click.option("--cit_cliente_id", help="El id del cliente.", type=int)
+def crear(cit_cliente_id):
+    """Crear una nueva encuesta de sistemas"""
+    click.echo(f"Crear una nueva encuesta de sistemas para el cliente con ID {cit_cliente_id}")
+
+    # Validar el cliente
+    cliente = CitCliente.query.get(cit_cliente_id)
     if cliente is None:
-        click.echo(click.style(f"El cliente con el id '{cliente_id}' no existe.", fg="red"))
+        click.echo(click.style(f"El cliente con el id '{cit_cliente_id}' no existe.", fg="red"))
         return 0
+
+    # Agregar la encuesta
     encuesta = EncSistema(
         cit_cliente=cliente,
         estado="PENDIENTE",
     )
     encuesta.save()
-    click.echo(f"Encuesta creada exitosamente con el id: {encuesta.id}")
+
+    # Mostrar el mensaje de termino
+    click.echo(f"Se ha creado la encuesta de sistemas con el id: {encuesta.id}")
 
 
 cli.add_command(consultar)
