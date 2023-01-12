4ada3312c7b3fe2cd8ddbaab1ad46787454f7dd4
Guillermo Valdes <guillermo.valdes@pjecz.gob.mx>
2023-01-12 12:08:30 -0600

    Folio

--- citas_admin/blueprints/pag_pagos/models.py
+++ citas_admin/blueprints/pag_pagos/models.py
@@ -31,10 +31,11 @@ class PagPago(db.Model, UniversalMixin):
     pag_tramite_servicio = db.relationship("PagTramiteServicio", back_populates="pag_pagos")
 
     # Columnas
-    total = db.Column(db.Numeric(precision=8, scale=2, decimal_return_scale=2), nullable=False)
     estado = db.Column(db.Enum(*ESTADOS, name="estados", native_enum=False), nullable=False)
-    email = db.Column(db.String(256))  # Email opcional si el cliente desea que se le envie el comprobante a otra dirección
-    ya_se_envio_comprobante = db.Column(db.Boolean, default=False)
+    email = db.Column(db.String(256), nullable=False, default="", server_default="")  # Email opcional si el cliente desea que se le envie el comprobante a otra dirección
+    folio = db.Column(db.String(256), nullable=False, default="", server_default="")
+    total = db.Column(db.Numeric(precision=8, scale=2, decimal_return_scale=2), nullable=False)
+    ya_se_envio_comprobante = db.Column(db.Boolean, nullable=False, default=False, server_default="false")
 
     def __repr__(self):
         """Representación"""
